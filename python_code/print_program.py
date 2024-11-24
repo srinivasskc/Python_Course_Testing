4273515f7883642e607b1394431308a58d8ed272
Srinivas <srinivas.skc30@gmail.com>
2024-11-24 09:12:34 +0530

    programs 24 Nov

---
+++ python_code/print_program.py
@@ -0,0 +1,29 @@
+# print(*args, sep=' ', end='\n', file=None, flush=False)
+
+print()
+
+# This will print with space in between as default
+print(1,2)
+
+# This will print with Tab Space in between as default
+print(1,2,sep='\t')
+
+# This will print in new lines
+print(3)
+print(4)
+print("Hi")
+
+# This will print in same line with space in between. (5 & 6 and ends with no space)
+print(5, end=' ')
+print(6, end='')
+
+# Print the information to File. "srinivas.txt"
+# fp is file pointer
+
+fp = open('srinivas.txt','w')
+print(1,2, file=fp)
+
+# Flush
+print("Hello - This is flush - false", flush=False);
+print('Official repository of "SAMURAI: Adapting Segment Anything Model for Zero-Shot Visual Tracking with Motion-Aware Memory"',flush=False);
+
---
+++ python_code/swap_variables.py
@@ -0,0 +1,15 @@
+print("Start of the swap program")
+
+a=5
+b=3
+
+print("Before swapping of variable a: ",a)
+print("Before swapping of variable b: ",b)
+
+temp = a
+print("value of a stored in temp variable: ",temp)
+a = b
+print("After swapping of variable a: ",a)
+b = temp
+print("After swapping of variable b: ",b)
+
